124bbeed728c952412c59100de43a5070447bfa2
holdyeah <2636403560@qq.com>
2019-10-08 23:13:05 +0800

    说明

--- README.md
+++ README.md
@@ -5,7 +5,9 @@
 微信的版本：2.6.8.56   
 开发环境：VS2017 使用C++的桌面开发   
 如何运行：在TeachDemos\Debug\下找到TeachDemos.exe以管理员运行,TeachDemos.exe(默认微信路径是)会加载同目录下的SendMessage.dll到微信中去   
+或者修改python_hook.py内的dll_path路径运行python_hook.py 可以注入dll到微信中去
 大神的https://github.com/hedada-hc/pc_wechat_hook   
+
 说明：TeachDemos.exe和微信exe需要以管理员运行，在微信目录下新建两个txt文件，write.txt编码UTF-8 read.txt编码UTF-8。不然程序无法读取写入到txt中去   
 
 代码和结构非常简陋   
@@ -18,3 +20,7 @@ web_wx.py不用下载，这个是我自己的获取的特定web,收发信息的p
 ![image](https://github.com/holdyeah/wechat-pc-hook-python/blob/master/images/TeachDemos.png)   
 内容显示   
 ![image](https://github.com/holdyeah/wechat-pc-hook-python/blob/master/images/SendMessage.png)   
+内容显示介绍：id/wxid是 wxid_****的一个数据 这个id/wxid 需要通过自己去获取，目前还没有增加到代码里面   
+发送的内容不需要填写，这个内容写在write.txt中去，dll就能读到发送到wxid去   
+大的框是实时信息获取的状态框，内容同时也写入到read.txt中去   
+操作方法：先点击获取信息 然后在id/wxid 填写数据 然后点击发送消息即可
---
+++ python_hook.py
@@ -0,0 +1,44 @@
+
+#-*- coding: utf-8 -*-
+import win32api,win32process,win32con,ctypes,psutil
+from ctypes import * 
+
+PAGE_READWRITE = 0x00000040
+PROCESS_ALL_ACCESS =  (0x000F0000|0x00100000|0xFFF)
+VIRTUAL_MEM = (0x00001000 | 0x00002000)
+dll_path = b"C:\SendMessage.dll"
+print(dll_path)
+dll_len = len(dll_path)
+kernel32 = ctypes.windll.kernel32
+#第一步获取整个系统的进程快照
+pids = psutil.pids()
+#第二步在快照中去比对进程名
+for pid in pids:
+    p= psutil.Process(pid)
+    #print(p.name())
+    if p.name()=='WeChat.exe':
+        print('%x'%pid)
+        break
+    else:
+        pid = 0
+#第三步用找到的pid去打开进程获取到句柄
+if pid == 0:
+    print("没有找到微信或者微信没有启动")
+else:
+    h_process=kernel32.OpenProcess(PROCESS_ALL_ACCESS,False,(pid))
+    print('%x'%h_process)
+    if not h_process:
+        print('进程打开失败权限不足')
+    else:
+        arg_adress=kernel32.VirtualAllocEx(h_process,None,dll_len*10,VIRTUAL_MEM,PAGE_READWRITE)
+        print('%x'%arg_adress)
+        NULL = c_int(0)
+        whhh=kernel32.WriteProcessMemory(h_process,arg_adress,dll_path,dll_len*10,NULL)
+        h_kernel32 = win32api.GetModuleHandle("kernel32.dll")
+        h_loadlib = win32api.GetProcAddress(h_kernel32, 'LoadLibraryA')
+        print('%x'%h_kernel32,'%x'%h_loadlib)
+        thread_id = c_ulong(0)
+        c_remt = kernel32.CreateRemoteThread(h_process,None,0,h_loadlib,arg_adress,0,byref(thread_id))
+        print('%x'%c_remt)
+        if not c_remt:
+            print("[!] Failed to inject DLL, exit...")
