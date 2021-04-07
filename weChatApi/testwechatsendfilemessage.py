0af00954d3ea3c01d9e4efe5e78960303735ffad
holdyeah <2636403560@qq.com>
2021-04-07 22:32:01 +0800

    文件发送demo

---
+++ weChatApi/testwechatsendfilemessage.py
@@ -0,0 +1,11 @@
+﻿import requests
+import urllib
+
+url = "http://127.0.0.1:2020/file"
+test_str = "D:/2.mp4"
+test22 = urllib.parse.quote(test_str)
+payload='wxid=filehelper&msg='+ test22
+print(payload)
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+response = requests.post(url, headers=headers, data=payload)
+print(response.text)
